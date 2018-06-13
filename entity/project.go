22feb51be065f8d2a592b20b13bdce423739d6fd
Nha Hoang <hdnha11@gmail.com>
2018-06-13 10:58:35 +0700

    Implement MongoDB repository

--- entity/project.go
+++ entity/project.go
@@ -6,7 +6,7 @@ import (
 
 // Project entity
 type Project struct {
-	ID          ID        `json:"id"`
+	ID          ID        `json:"id" bson:"_id"`
 	Name        string    `json:"name"`
 	Description string    `json:"description"`
 	CreatedAt   time.Time `json:"createdAt"`
--- main.go
+++ main.go
@@ -3,26 +3,40 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/julienschmidt/httprouter"
 	"github.com/red-pola/crypto-wallet-api/project"
+	"gopkg.in/mgo.v2"
 )
 
 func main() {
+	godotenv.Load()
+
+	// Connect to MongoDB
+	session, err := mgo.Dial(os.Getenv("MONGODB_HOST"))
+	if err != nil {
+		log.Fatal("Cannot connect to MongoDB: ", err)
+	}
+	defer session.Close()
+	db := session.DB(os.Getenv("MONGODB_DATABASE"))
+
 	router := httprouter.New()
 
 	// Projects
-	projectRepository := project.NewInMemRepository()
+	projectRepository := project.NewMongoRepository(db)
 	projectService := project.NewService(projectRepository)
 	project.InitRouters(router, projectService)
 
 	// Ping
-	router.GET("/status", func(res http.ResponseWriter, req *http.Request, params httprouter.Params) {
+	router.GET("/status", func(res http.ResponseWriter, req *http.Request, _ httprouter.Params) {
 		res.WriteHeader(http.StatusOK)
 	})
 
-	log.Println("CryptoWalletAPI is listening on port 8080")
-	if err := http.ListenAndServe(":8080", router); err != nil {
+	port := os.Getenv("PORT")
+	log.Printf("CryptoWalletAPI is listening on port %s", port)
+	if err := http.ListenAndServe(":"+port, router); err != nil {
 		log.Fatal(err)
 	}
 }
